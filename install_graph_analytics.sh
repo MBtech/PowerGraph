af44bf0e7b39dfb9a1a6e790fccb71228bd8a753
MBTech <mubil@kth.se>
2019-06-27 00:41:24 +0000

    Fix for zookeeper makefile flag

--- install_graph_analytics.sh
+++ install_graph_analytics.sh
@@ -2,4 +2,6 @@ sudo apt-get update
 sudo apt-get install gcc g++ build-essential libopenmpi-dev openmpi-bin default-jdk cmake zlib1g-dev git libomp-dev libboost-all-dev gdb
 ./configure
 cd release/toolkits/graph_analytics
-make -j4
+# Set the flag here to override the flag in zookeeper makefile
+export AM_CFLAGS="-Wall -Wdeclaration-after-statement"
+make -e -j4
